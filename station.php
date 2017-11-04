78d7e78b5ef719a50efbfac9655285aac4bac696
maeda yoshiya <hikowaiide@gmail.com>
2017-11-05 01:12:12 +0900

    test25

--- station.php
+++ station.php
@@ -1,6 +1,8 @@
 <?php
-$word = $_GET['area'];
-$url = "https://api.ekispert.jp/v1/xml/geo/station?key=eBBWPyXMYduCN759&geoPoint=35.6729,139.768962,500";
-$hoge = file_get_contents($url);
+$latitude = $_GET['latitude'];
+$longitude = $_GET['longitude'];
+
+$url = "https://api.ekispert.jp/v1/xml/geo/station?key=eBBWPyXMYduCN759&geoPoint=$latitude,$longitude,500";
 $responce = new SimpleXMLElement(file_get_contents($url));
-echo json_encode($responce->Point->Station->Name);
+
+echo json_encode($responce->Point->Station->Name, JSON_UNESCAPED_UNICODE);
