0d605838477f6f8fd9625405ab29af253a149ec5
maeda yoshiya <hikowaiide@gmail.com>
2017-11-05 00:08:07 +0900

    test13

--- gnavi.php
+++ gnavi.php
@@ -1,4 +1,4 @@
 <?php
-$url = "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=758c6cf7dfeaf15b08a8c50bb37c02b3&format=xml&freeword=肉&areacode_m=AREAM2105";
+$url = "https://api.gnavi.co.jp/RestSearchAPI/20170630/?keyid=758c6cf7dfeaf15b08a8c50bb37c02b3&format=xml&freeword=肉&areacode_m=AREAM2105&hit_per_page=1";
 $responce = new SimpleXMLElement(file_get_contents($url));
-var_dump($responce["total_hit_count"]);
+var_dump($responce->rest->url);
