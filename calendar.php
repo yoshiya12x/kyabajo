d2434dc83ac521b238b6304e85984421ce88197e
maeda yoshiya <hikowaiide@gmail.com>
2017-11-05 03:32:09 +0900

    test32

--- calendar.php
+++ calendar.php
@@ -1,12 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
-mb_internal_encoding("UTF-8");
 
-$tommorow = date("Y-m-d 03:07:00");//,strtotime("+1 day")
+$tommorow = date("Y-m-d 17:00:00",strtotime("+1 day"));
 $unixTommorow = strtotime($tommorow);
 
 $slackApiKey = "xoxp-166232831345-166209035744-266988840611-5b7acc4e2912ff45287909ecb9736934";
-$alertText = "あいうえお";
+$alertText = "18:00dinner";
 $url = "https://slack.com/api/reminders.add?token=${slackApiKey}&text=${alertText}&time=${unixTommorow}";
 
 file_get_contents($url);
--- gnavi.php
+++ gnavi.php
@@ -2,4 +2,4 @@
 $word = $_GET['food'];
 $url = "https://api.gnavi.co.jp/RestSearchAPI/20170630/?keyid=758c6cf7dfeaf15b08a8c50bb37c02b3&format=xml&freeword=$word&areacode_m=AREAM2105&hit_per_page=1";
 $responce = new SimpleXMLElement(file_get_contents($url));
-echo json_encode(urldecode($responce->rest->url));
+echo json_encode($responce->rest->url);
